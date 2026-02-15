972f09fc835ce1ec05e94722aa22354b47f08005
micheal965 <micheal.magdy69@yahoo.com>
2026-02-15 04:35:44 +0200

    feat: implement landing page sections and refined grid logic

--- SmartDonationSystemClient/src/app/features/home/pages/landing-page/landing-page.component.ts
+++ SmartDonationSystemClient/src/app/features/home/pages/landing-page/landing-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { AboutUsComponent } from '../../components/about-us/about-us.component';
 import { OurTeamComponent } from '../../components/our-team/our-team.component';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -16,11 +15,4 @@ import { RouterLink } from '@angular/router';
   ],
   templateUrl: './landing-page.component.html',
 })
-export class LandingPageComponent {
-  scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
-  }
-}
+export class LandingPageComponent {}
