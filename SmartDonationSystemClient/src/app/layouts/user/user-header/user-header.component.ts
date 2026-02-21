77383465f53d615eb1856a60fc8428c2e15e05eb
micheal965 <micheal.magdy69@yahoo.com>
2026-02-21 04:16:35 +0200

    style(user-authenticated-layout): refine styles in authenticated user layout and related components

--- SmartDonationSystemClient/src/app/layouts/user/user-header/user-header.component.ts
+++ SmartDonationSystemClient/src/app/layouts/user/user-header/user-header.component.ts
@@ -1,12 +1,12 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-user-header',
   standalone: true,
-  imports: [RouterLink, NgIf, NgFor, MatIconModule],
+  imports: [RouterLink, NgIf, NgFor, MatIconModule, RouterLinkActive],
   templateUrl: './user-header.component.html',
   styleUrl: './user-header.component.scss',
 })
@@ -29,6 +29,16 @@ export class UserHeaderComponent {
       label: 'Notifications',
       link: '/notifications',
       iconName: 'notifications',
+    },
+    {
+      label: 'My Donations',
+      link: '/donations',
+      iconName: 'favorite',
+    },
+    {
+      label: 'Categories',
+      link: '/categories',
+      iconName: 'grid_view',
     },
   ];
   toggleMe() {
--- SmartDonationSystemClient/src/app/layouts/user/user-left-sidebar/user-left-sidebar.component.ts
+++ SmartDonationSystemClient/src/app/layouts/user/user-left-sidebar/user-left-sidebar.component.ts
@@ -3,11 +3,11 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
-import { RouterLinkActive } from "@angular/router";
+import { RouterLinkActive, RouterLink } from "@angular/router";
 @Component({
   selector: 'app-user-left-sidebar',
   standalone: true,
-  imports: [MatListModule, MatIconModule, MatButtonModule, MatDividerModule, RouterLinkActive],
+  imports: [MatListModule, MatIconModule, MatButtonModule, MatDividerModule, RouterLinkActive, RouterLink],
   templateUrl: './user-left-sidebar.component.html',
   styleUrl: './user-left-sidebar.component.scss',
 })
