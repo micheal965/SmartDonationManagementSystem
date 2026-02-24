0d7798b0f386ef796401ac3d40e02e814bfaafde
micheal965 <micheal.magdy69@yahoo.com>
2026-02-24 08:27:09 +0200

    chore(config): update API base URL to production environment

--- SmartDonationSystemClient/src/app/core/utils/app.config.ts
+++ SmartDonationSystemClient/src/app/core/utils/app.config.ts
@@ -1,6 +1,6 @@
 export const apiBaseUrl =
-  // 'https://smartdonationmanagementsystem.runasp.net/api';
-'https://localhost:7190/api';
+  'https://smartdonationmanagementsystem.runasp.net/api';
+// 'https://localhost:7190/api';
 
 export const AiEnvironment = {
   apiKey: 'AIzaSyDSJXuan4aG7VM0lfV8NAhGExe6rsuCWVI',
