1df3452f47895fbecb5d0b1b5ffa5a4d5fc40117
micheal965 <micheal.magdy69@yahoo.com>
2026-03-25 19:37:36 +0200

    Update binaries and assembly information for SmartDonationSystem projects
    - Updated the assembly informational version for SmartDonationSystem.API, SmartDonationSystem.Core, SmartDonationSystem.DataAccess, and SmartDonationSystem.Services to reflect new versioning.
    - Regenerated binary files including .dll, .pdb, and .exe for all projects.
    - Updated sourcelink JSON files to point to the new commit hashes.
    - Updated various cache files to ensure proper project references and compilation inputs.

--- SmartDonationSystemSolution/SmartDonationSystem.Core/obj/Release/net8.0/SmartDonationSystem.Core.AssemblyInfo.cs
+++ SmartDonationSystemSolution/SmartDonationSystem.Core/obj/Release/net8.0/SmartDonationSystem.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartDonationSystem.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01674bfd3edea8d3fb9ec888780e34debbc5d628")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee4b493690f1e317f9c77c453c09523fe14cac9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartDonationSystem.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartDonationSystem.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SmartDonationSystemSolution/SmartDonationSystem.Services/obj/Release/net8.0/SmartDonationSystem.Services.AssemblyInfo.cs
+++ SmartDonationSystemSolution/SmartDonationSystem.Services/obj/Release/net8.0/SmartDonationSystem.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartDonationSystem.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01674bfd3edea8d3fb9ec888780e34debbc5d628")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee4b493690f1e317f9c77c453c09523fe14cac9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartDonationSystem.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartDonationSystem.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
